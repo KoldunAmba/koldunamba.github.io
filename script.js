2c217759de721669b703d97635784cb854c02f01
KoldunAmba <koldunamba@gmail.com>
2022-01-10 15:22:43 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,73 @@
-var squares = document.getElementById("anim");
-var animation = bodymovin.loadAnimation({
-	container: document.getElementById('anim'),
+var animation1 = bodymovin.loadAnimation({
+	container: document.getElementById('anim1'),
 	renderer: 'svg',
 	loop: true,
 	autoplay: false,
 	path: 'lottie/speedometer.json'
-})
+});
+
+var animation2 = bodymovin.loadAnimation({
+	container: document.getElementById('anim2'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: false,
+	path: 'lottie/scales.json'
+});
+
+var animation3 = bodymovin.loadAnimation({
+	container: document.getElementById('anim3'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: false,
+	path: 'lottie/figures.json'
+});
+
+var animation4 = bodymovin.loadAnimation({
+	container: document.getElementById('anim4'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: false,
+	path: 'lottie/variability.json'
+});
+
+var animation5 = bodymovin.loadAnimation({
+	container: document.getElementById('anim5'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: false,
+	path: 'lottie/camera.json'
+});
+
+var animation6 = bodymovin.loadAnimation({
+	container: document.getElementById('anim6'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: false,
+	path: 'lottie/gift.json'
+});
 
 squares.addEventListener("mouseenter", function () {
-    animation.play();
+    animation1.play();
+});
+
+squares.addEventListener("mouseenter", function () {
+    animation2.play();
+});
+
+squares.addEventListener("mouseenter", function () {
+    animation3.play();
+});
+
+squares.addEventListener("mouseenter", function () {
+    animation4.play();
+});
+
+squares.addEventListener("mouseenter", function () {
+    animation5.play();
+});
+
+squares.addEventListener("mouseenter", function () {
+    animation6.play();
 });
 
 function playAnim(anim, loop) {
@@ -18,13 +77,8 @@ function playAnim(anim, loop) {
     }
 }
 
-squares.addEventListener("mouseleave", function () {
-  animation.gotoAndStop(0);
-});
-
 function pauseAnim(anim) {
     if (!anim.isPaused) {
         anim.loop = false;
     }
 }
-
