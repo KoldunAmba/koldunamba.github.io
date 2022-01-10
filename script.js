32dd3392c73264d11e7e5563aae42f30f6177731
KoldunAmba <koldunamba@gmail.com>
2022-01-10 15:08:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+var squares = document.getElementById("anim");
 var animation = bodymovin.loadAnimation({
 	container: document.getElementById('anim'),
 	renderer: 'svg',
 	loop: true,
-	autoplay: true,
+	autoplay: false,
 	path: 'lottie/speedometer.json'
 })
+
+squares.addEventListener("mouseenter", function () {
+    animation.play();
+});
+
+function playAnim(anim, loop) {
+    if(anim.isPaused) {
+        anim.loop = loop;
+        anim.goToAndPlay(0);
+    }
+}
+
+function pauseAnim(anim) {
+    if (!anim.isPaused) {
+        anim.loop = false;
+    }
+}
